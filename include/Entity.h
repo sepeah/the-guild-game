1b48812d175f7b72ae8c5c027b818579e65c7db7
Sepe <sepe.ahtosalo@gmail.com>
2026-02-25 13:07:40 +0200

    added methods to Monster class

--- include/Entity.h
+++ include/Entity.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <algorithm>
 
 class Renderable {
 public:
@@ -12,11 +13,17 @@ public:
 class Monster : public Renderable {
 private: 
     int health, damage;
-    std:string Monstername;
+    std::string name;
 public:
-    Monster(std::string Monstername)
-        : name(Monstername), name(levelName) {}
+    Monster(std::string Monstername, int startX, int startY, int hp, int dmg, char sym)
+        : name(Monstername), x(startX), y(startY), health(hp), damage(dmg), symbol(sym) {}
+    
+    void takeDamage(int damage) { 
+        health = std::max(0, health - damage); 
+    }
+    int getHealth() const {return health; }
+    int getDamage() const {return damage; }
+    std::string getName() const {return name; }
 
-
-
-}
+    bool isAlive() const { return health > 0; }
+};
